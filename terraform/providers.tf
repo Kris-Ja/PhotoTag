849ead91323fc212c706139c9c4f9711b8315609
Kris-Ja <107918765+Kris-Ja@users.noreply.github.com>
2026-06-10 17:13:44 +0200

    Cleanup terraform files

---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.76.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.9.0"
+    }
+  }
+}
+
