b13ad52c233ae9fe0b997bdc37536ee6b9f7a59d
Kris-Ja <107918765+Kris-Ja@users.noreply.github.com>
2026-06-16 00:59:42 +0200

    Add separate service plan for thumbnails-service

--- terraform/service_plan.tf
+++ terraform/service_plan.tf
@@ -21,3 +21,11 @@ resource "azurerm_service_plan" "metadata_service_plan" {
   os_type             = "Linux"
   sku_name            = "FC1"
 }
+
+resource "azurerm_service_plan" "thumbnails_service_plan" {
+  name                = "thumbnails-service-plan"
+  location            = azurerm_resource_group.phototag.location
+  resource_group_name = azurerm_resource_group.phototag.name
+  os_type             = "Linux"
+  sku_name            = "FC1"
+}
