f765089be0f227ee9847a3380722692d01b72146
Kris-Ja <107918765+Kris-Ja@users.noreply.github.com>
2026-06-15 18:36:38 +0200

    Refactor code

--- terraform/metadata-service.tf
+++ terraform/metadata-service.tf
@@ -1,3 +1,14 @@
+resource "azurerm_storage_container" "metadata_service" {
+  name                  = "metadata-service"
+  storage_account_id    = azurerm_storage_account.images.id
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_table" "metadata" {
+  name                 = "metadata"
+  storage_account_name = azurerm_storage_account.images.name
+}
+
 resource "azurerm_function_app_flex_consumption" "metadata_service" {
   name                = lower("metadata-service${random_id.random.hex}")
   resource_group_name = azurerm_resource_group.phototag.name
@@ -5,7 +16,7 @@ resource "azurerm_function_app_flex_consumption" "metadata_service" {
   service_plan_id     = azurerm_service_plan.metadata_service_plan.id
 
   storage_container_type        = "blobContainer"
-  storage_container_endpoint    = "${azurerm_storage_account.images.primary_blob_endpoint}${azurerm_storage_container.metadata_images.name}"
+  storage_container_endpoint    = "${azurerm_storage_account.images.primary_blob_endpoint}${azurerm_storage_container.metadata_service.name}"
   storage_authentication_type   = "StorageAccountConnectionString"
   storage_access_key            = azurerm_storage_account.images.primary_access_key
   runtime_name                  = "python"
@@ -18,12 +29,30 @@ resource "azurerm_function_app_flex_consumption" "metadata_service" {
   }
 
   app_settings = {
-    ENV_PHOTOS_CONNSTR                       = azurerm_storage_account.images.primary_connection_string
-    ENV_SERVICE_BUS_CONNSTR                  = azurerm_servicebus_namespace.image_namespace.default_primary_connection_string
-    ENV_SERVICE_BUS_NEW_IMAGE_TOPIC_NAME     = azurerm_servicebus_topic.new_image.name
-    ENV_SERVICE_BUS_NEW_THUMBNAIL_TOPIC_NAME = azurerm_servicebus_topic.new_thumbnail.name
-    ENV_SERVICE_BUS_NEW_TAGS_TOPIC_NAME      = azurerm_servicebus_topic.new_tags.name
-    ENV_METADATA_STORAGE_NAME                = azurerm_storage_table.metadata.name
-    ENV_METADATA_NEW_IMAGE_TOPIC_NAME        = azurerm_servicebus_topic.new_meta_image.name
+    ENV_PHOTOS_CONNSTR            = azurerm_storage_account.images.primary_connection_string
+    ENV_SERVICE_BUS_CONNSTR       = azurerm_servicebus_namespace.servicebus.default_primary_connection_string
+    ENV_NEW_IMAGE_TOPIC_NAME      = azurerm_servicebus_topic.new_image.name
+    ENV_NEW_IMAGE_BLOB_TOPIC_NAME = azurerm_servicebus_topic.new_image_blob.name
+    ENV_NEW_THUMBNAIL_TOPIC_NAME  = azurerm_servicebus_topic.new_thumbnail.name
+    ENV_NEW_TAGS_TOPIC_NAME       = azurerm_servicebus_topic.new_tags.name
+    ENV_METADATA_STORAGE_NAME     = azurerm_storage_table.metadata.name
   }
+}
+
+resource "azurerm_servicebus_subscription" "metadata_tags_sub" {
+  name               = "metadata-tags-sub"
+  topic_id           = azurerm_servicebus_topic.new_tags.id
+  max_delivery_count = 5
+}
+
+resource "azurerm_servicebus_subscription" "metadata_image_blob_sub" {
+  name               = "metadata-image-blob-sub"
+  topic_id           = azurerm_servicebus_topic.new_image_blob.id
+  max_delivery_count = 5
+}
+
+resource "azurerm_servicebus_subscription" "metadata_thumbnail_sub" {
+  name               = "metadata-thumbnail-sub"
+  topic_id           = azurerm_servicebus_topic.new_thumbnail.id
+  max_delivery_count = 5
 }
